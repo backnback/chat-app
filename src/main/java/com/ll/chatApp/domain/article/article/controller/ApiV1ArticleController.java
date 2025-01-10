cc5b9eb3b3f2113fdce8787f62bf0ba5dfa59e04
backnback <garam8796@naver.com>
2025-01-10 11:17:06 +0900

    b

---
+++ src/main/java/com/ll/chatApp/domain/article/article/controller/ApiV1ArticleController.java
@@ -0,0 +1,58 @@
+package com.ll.chatApp.domain.article.article.controller;
+
+
+import com.ll.chatApp.domain.article.article.dto.ArticleDto;
+import com.ll.chatApp.domain.article.article.dto.ArticleWriteRequest;
+import com.ll.chatApp.domain.article.article.entity.Article;
+import com.ll.chatApp.domain.article.article.service.ArticleService;
+import com.ll.chatApp.global.rsData.RsData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/articles")
+@RequiredArgsConstructor
+public class ApiV1ArticleController {
+  private final ArticleService articleService;
+
+  @GetMapping
+  public List<ArticleDto> getArticles() {
+    List<Article> articles = articleService.findAll();
+
+    List<ArticleDto> articleDtoList = articles.stream()
+        .map(ArticleDto::new)
+        .toList();
+
+    return articleDtoList;
+  }
+
+  @GetMapping({"/{id}"})
+  private ArticleDto getArticle(@PathVariable("id") Long id) {
+    Article article = articleService.findById(id).orElseGet(Article::new);
+
+    return new ArticleDto(article);
+  }
+
+  @PostMapping
+  public RsData<ArticleDto> writeArticle(@RequestBody ArticleWriteRequest articleWriteRequest) {
+    Article article = articleService.write(articleWriteRequest.getTitle(), articleWriteRequest.getContent());
+
+    return RsData.of(
+        "200",
+        "게시글이 작성에 성공하였습니다.",
+        new ArticleDto(article)
+    );
+  }
+
+  @PatchMapping({"/{id}"})
+  public void updateArticle(@PathVariable("id") Long id, @RequestBody Article article) {
+    this.articleService.modify(article, article.getTitle(), article.getContent());
+  }
+
+  @DeleteMapping({"/{id}"})
+  public void deleteArticle(@PathVariable("id") Long id) {
+    this.articleService.delete(id);
+  }
+}
